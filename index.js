7aff56aded3d6fbd9ad79fc7b0ce7f1761ee2805
udisha chaudhary <udishachaudhary99@gmail.com>
2024-02-19 00:53:16 +0530

    new design scratch

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+
+const app = new express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.set('view engine', 'ejs')
+
+app.listen(3050,()=>{
+    console.log("App is listening on port 3050")
+})
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+
+
