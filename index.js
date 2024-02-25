a1a9e678cc1074fb4830e71caa52e178b4fa09bb
udisha chaudhary <udishachaudhary99@gmail.com>
2024-02-25 16:36:49 +0530

    add extra slash to path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded())
 
 app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
